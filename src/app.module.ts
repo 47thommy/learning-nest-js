561c64beb9ff749e8f86f4d82ca14523739bade6
47thommy <47thommy@gmail.com>
2024-09-07 14:19:55 +0300

    Asynchronously Configure Dynamic Modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,6 +10,18 @@ import appConfig from './config/app.config';
 
 @Module({
   imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.DATABASE_HOST,
+        port: +process.env.DATABASE_PORT,
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        autoLoadEntities: true,
+        synchronize: true, // should be disabled for production
+      }),
+    }),
     ConfigModule.forRoot({
       // envFilePath:".envrionmet" to specify a different path, by default the config module looks for .env file in the root directory
       // ignoreEnvFile: true, later when we deploy our app .env will not be used so we can tell the config module not to load it
@@ -21,16 +33,6 @@ import appConfig from './config/app.config';
       load: [appConfig],
     }),
     CoffeesModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: +process.env.DATABASE_PORT,
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
-      autoLoadEntities: true,
-      synchronize: true, // should be disabled for production
-    }),
     CoffeeRatingModule,
   ],
   controllers: [AppController],
